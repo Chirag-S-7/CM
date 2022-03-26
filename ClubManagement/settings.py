358913363de4ee5d0a20ac8121b3ba0f7a6c5ba1
Chirag Shah <chiragladdha9104@gmail.com>
2022-03-26 10:54:24 +0530

    First Commit

--- ClubManagement/settings.py
+++ ClubManagement/settings.py
@@ -120,10 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(ClubManagement, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# django_heroku.settings(locals())
+
--- ClubManagement/urls.py
+++ ClubManagement/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls.static import static
+
 # from django.conf.urls import url
 
 admin.site.site_header = "YourEvent Admin"
@@ -26,6 +27,6 @@ admin.site.index_title = "Welcome to YourEvent Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clubM.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
